13c8ff3e2aed9454208f51d35418143b4e3cd003
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-05-28 19:04:19 +0700

    index.js: enable flow
    Also fix a minor warning.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+if (!rootElement)
+    throw new Error('Root container not found. Script might load too fast or you may forget to put it in HTML.');
+
+ReactDOM.render(<App />, rootElement);
