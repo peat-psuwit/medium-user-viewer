ebfc8ba498ece1c2094a78325dfb0854b90976b6
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-05-31 17:54:24 +0700

    utils: mediumAPI: UserProfileType

--- src/utils/mediumAPI.js
+++ src/utils/mediumAPI.js
@@ -1,3 +1,11 @@
 // @flow
 
 export const MEDIUM_API_BASE_URL = 'http://localhost:8080/https://api.medium.com/v1';
+
+export type UserProfileType = {
+    id: string,
+    username: string,
+    name: string,
+    url: string,
+    imageUrl: string
+};
