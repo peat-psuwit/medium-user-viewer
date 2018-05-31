1f210e22e26128ff9110d2aebfe847b27228b079
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-05-31 18:24:05 +0700

    actions: userProfileActions: fetchUserProfile

--- src/actions/userProfileActions.js
+++ src/actions/userProfileActions.js
@@ -1,5 +1,9 @@
 // @flow
 
+import { getMe } from '../utils/mediumAPI';
+
+import type { StateType } from '../reducers';
+import type { ActionsType } from './';
 import type { UserProfileType } from '../utils/mediumAPI';
 
 function userProfileFetchStarted() {
@@ -20,6 +24,27 @@ function userProfileFetchedFailed(error: Error) {
         type: 'USER_PROFILE_FETCH_FAILED',
         error
     };
+}
+
+export function fetchUserProfile() {
+    return function (dispatch: (ActionsType) => Promise<*>, getState: () => StateType) {
+        const { auth } = getState();
+
+        if (!auth || !auth.currentToken)
+            return Promise.reject(new Error('fetchUserProfile: not authenticated'));
+
+        const currentToken = auth.currentToken;
+
+        // $FlowFixMe: still WTF
+        dispatch(userProfileFetchStarted());
+        return getMe(currentToken).then( (userProfile) => {
+            // $FlowFixMe: still WTF
+            dispatch(userProfileFetchedSuccess(userProfile));
+        }).catch( (error) => {
+            // $FlowFixMe: still WTF
+            dispatch(userProfileFetchedFailed(error));
+        });
+    }
 }
 
 export type UserProfileActionsType = $Call<typeof userProfileFetchStarted>
