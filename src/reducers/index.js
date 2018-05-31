63f45bc8e7e4452a04ee0a1cd39ad70bd6ee1df2
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-05-31 18:41:21 +0700

    reducers: publicatonListReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,15 +4,18 @@ import { combineReducers } from 'redux';
 
 import authReducer from './authRecucer';
 import userProfileReducer from './userProfileReducer';
+import publicationListReducer from './publicationListReducer';
 
 import type { ActionsType } from '../actions';
 
 export default combineReducers({
     auth: authReducer,
-    userProfile: userProfileReducer
+    userProfile: userProfileReducer,
+    publicationList: publicationListReducer
 });
 
 export type StateType = {
     auth: $Call<typeof authReducer, *, ActionsType>,
-    userProfile: $Call<typeof userProfileReducer, *, ActionsType>
+    userProfile: $Call<typeof userProfileReducer, *, ActionsType>,
+    publicationList: $Call<typeof publicationListReducer, *, ActionsType>
 };
---
+++ src/reducers/publicationListReducer.js
@@ -0,0 +1,44 @@
+// @flow
+
+import type { PublicationListType } from '../utils/mediumAPI';
+import type { ActionsType } from '../actions';
+
+type PublicationListState = {
+    isLoading: boolean,
+    data: ?PublicationListType,
+    error: ?Error
+};
+
+const defaultState: PublicationListState = {
+    isLoading: false,
+    data: null,
+    error: null
+};
+
+export default function publicationListReducer(
+    previousState: PublicationListState = defaultState,
+    // $FlowFixMe: All branches are incompatible, but it's union?
+    action: ActionsType
+): PublicationListState {
+    switch (action.type) {
+        case 'PUBLICATION_LIST_FETCH_STARTED':
+            return {
+                ...previousState,
+                isLoading: true
+            };
+        case 'PUBLICATION_LIST_FETCH_SUCCESS':
+            return {
+                isLoading: false,
+                data: action.data,
+                error: null
+            };
+        case 'PUBLICATION_LIST_FETCH_FAILED':
+            return {
+                isLoading: false,
+                data: null,
+                error: action.error
+            };
+        default:
+            return previousState;
+    }
+}
