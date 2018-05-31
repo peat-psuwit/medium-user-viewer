df6c9d3bd1da55326080330b22438da09532b527
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-05-31 18:05:12 +0700

    utils: mediumAPI: getMe

--- src/utils/mediumAPI.js
+++ src/utils/mediumAPI.js
@@ -1,6 +1,16 @@
 // @flow
 
+import axios from 'axios';
+
+import type { OAuthToken } from './mediumAuth';
+
 export const MEDIUM_API_BASE_URL = 'http://localhost:8080/https://api.medium.com/v1';
+
+function getAuthorizationHeader(token: OAuthToken) {
+    return {
+        'Authorization': `Bearer ${token.access_token}`
+    };
+}
 
 export type UserProfileType = {
     id: string,
@@ -9,3 +19,9 @@ export type UserProfileType = {
     url: string,
     imageUrl: string
 };
+
+export function getMe(token: OAuthToken): Promise<UserProfileType> {
+    return axios.get(`${MEDIUM_API_BASE_URL}/me`, {
+        headers: getAuthorizationHeader(token)
+    }).then( (response) => response.data);
+}
