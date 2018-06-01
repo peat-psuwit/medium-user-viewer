100febec88cb71cde099bd280f7f3290bd80522d
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 10:27:12 +0700

    actions: publicationListActions: fetchPublicationList

--- src/actions/publicationListActions.js
+++ src/actions/publicationListActions.js
@@ -1,5 +1,9 @@
 // @flow
 
+import { getUserPublication } from '../utils/mediumAPI';
+
+import type { StateType } from '../reducers';
+import type { ActionsType } from './';
 import type { PublicationListType } from '../utils/mediumAPI';
 
 function publicationListFetchStarted() {
@@ -20,6 +24,32 @@ function publicationListFetchedFailed(error: Error) {
         type: 'PUBLICATION_LIST_FETCH_FAILED',
         error
     };
+}
+
+export function fetchPublicationList() {
+    return function (dispatch: (ActionsType) => Promise<*>, getState: () => StateType) {
+        const { auth, userProfile } = getState();
+
+        if (!auth || !auth.currentToken)
+            return Promise.reject(new Error('fetchPublicationList: not authenticated'));
+
+        const currentToken = auth.currentToken;
+
+        if (!userProfile.data)
+            return Promise.reject(new Error('fetchPublicationList: userId unknown'));
+
+        const userId = userProfile.data.id;
+
+        // $FlowFixMe: still WTF
+        dispatch(publicationListFetchStarted());
+        return getUserPublication(currentToken, userId).then( (publicationList) => {
+            // $FlowFixMe: still WTF
+            dispatch(publicationListFetchedSuccess(publicationList));
+        }).catch( (error) => {
+            // $FlowFixMe: still WTF
+            dispatch(publicationListFetchedFailed(error));
+        });
+    }
 }
 
 export type PublicationListActionsType = $Call<typeof publicationListFetchStarted>
