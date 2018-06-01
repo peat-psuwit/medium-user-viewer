55e097da20d214722a37a20a3e6ea87fc0565d5b
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 15:46:55 +0700

    pages: HomePage: UserProfile

---
+++ src/pages/HomePage/UserProfile.js
@@ -0,0 +1,32 @@
+// @flow
+
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+
+import type { UserProfileType } from "../../utils/mediumAPI";
+
+import './UserProfile.css';
+
+type PropsType = {
+    userProfile: UserProfileType
+};
+
+export default function UserProfile({ userProfile }: PropsType) {
+    return (
+        <Row>
+            <Col sm={{ size: 3 }} className="text-center" >
+                <img className="UserProfile_ProfileImage rounded-circle"
+                    src={userProfile.imageUrl}
+                    alt={`Image of ${userProfile.name}`}
+                />
+            </Col>
+
+            <Col sm={{ size: 9 }} >
+                <a href={userProfile.url} target="_blank">
+                    <h3>{userProfile.name}</h3>
+                    <span>{userProfile.username}</span>
+                </a>
+            </Col>
+        </Row>
+    );
+}
