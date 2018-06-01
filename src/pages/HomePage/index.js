7b2cc9cd49e50b7680deb17980f94a2edf2bced7
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 15:50:36 +0700

    pages: HomePage: show UserProfile

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -3,10 +3,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class HomePage extends Component<*> {
-    render() {
-        return <h1>HomePage</h1>
+import type { ContextRouter } from 'react-router-dom';
+
+import { fetchAll } from '../../actions';
+
+import type { StateType } from '../../reducers';
+
+import UserProfile from './UserProfile';
+
+function mapStateToProp(state: StateType) {
+    return {
+        userProfile: state.userProfile
     }
 }
 
-export default HomePage;
+const dispatchToPropMap = {
+    fetchAll
+};
+
+type PropsType = {
+    ...$Call<typeof mapStateToProp, *>,
+    ...typeof dispatchToPropMap,
+    ...ContextRouter
+};
+
+class HomePage extends Component<PropsType> {
+    componentDidMount() {
+        this.props.fetchAll();
+    }
+
+    render() {
+        if (!this.props.userProfile.data)
+            // TODO: spinner
+            return <div>Loading...</div>
+
+        return (
+            <div>
+                <UserProfile userProfile={this.props.userProfile.data} />
+            </div>
+        );
+    }
+}
+
+export default connect(mapStateToProp, dispatchToPropMap)(HomePage);
