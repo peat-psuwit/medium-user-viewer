db5b1a892d31c7363266805d68897822499cf050
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 12:22:30 +0700

    App.js: initialise React-Router & Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Container } from 'reactstrap';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <BrowserRouter>
+        <Container>
+          TODO
+        </Container>
+      </BrowserRouter>
     );
   }
 }
