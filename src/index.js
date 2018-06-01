7e6d45c7c08816cdbe57c1c1e59762bac7de9328
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 12:01:06 +0700

    index: provide Redux store into React app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import reducers from './reducers';
@@ -23,4 +24,8 @@ const rootElement = document.getElementById('root');
 if (!rootElement)
     throw new Error('Root container not found. Script might load too fast or you may forget to put it in HTML.');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, rootElement);
