10d2594ae70d17bc0d5e9c8c76d7d5ef379f8fcb
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 13:10:06 +0700

    pages: stub HomePage and AuthPage

---
+++ src/pages/AuthPage.js
@@ -0,0 +1,12 @@
+// @flow
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class AuthPage extends Component<*> {
+    render() {
+        return <h1>AuthPage</h1>
+    }
+}
+
+export default AuthPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+// @flow
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class HomePage extends Component<*> {
+    render() {
+        return <h1>HomePage</h1>
+    }
+}
+
+export default HomePage;
