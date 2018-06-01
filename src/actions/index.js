91125026d7240aceb7f9882f19c40cae9152fba0
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 11:42:18 +0700

    actions: ActionsType: add thunk action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 // @flow
 
+import type { StateType } from '../reducers';
+
 import type { AuthActionsType } from "./authActions";
 import type { UserProfileActionsType } from './userProfileActions';
 import type { PublicationListActionsType } from './publicationListActions';
@@ -8,4 +10,6 @@ import type { PubContribsActionsType } from './publicationContributorsActions';
 export type ActionsType = AuthActionsType
                         | UserProfileActionsType
                         | PublicationListActionsType
-                        | PubContribsActionsType;
+                        | PubContribsActionsType
+                        // Thunk
+                        | (dispatch: (ActionsType) => Promise<*>, getState: () => StateType) => Promise<any>;
