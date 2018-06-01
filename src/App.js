b9e4c9694855b03d1f9c947bf3bce7cedb329910
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 14:15:13 +0700

    App: redirect-to-auth infrastructure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
+// @flow
+
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Container } from 'reactstrap';
+import { connect } from 'react-redux';
+
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+
+import type { StateType } from './reducers';
+import PrivateRoute from './utils/PrivateRoute';
 
 import './App.css';
 
 export const LOGIN_PATH = '/login';
 
-class App extends Component {
+function mapStateToProp(state: StateType) {
+  return {
+    auth: state.auth
+  };
+}
+
+class App extends Component<$Call<typeof mapStateToProp, *>> {
   render() {
+    if (!this.props.auth)
+      // TODO: spinner
+      return <h1>Loading...</h1>
+
     return (
       <BrowserRouter>
         <Container>
-          TODO
+          <Switch>
+            <Route path={LOGIN_PATH} component={AuthPage} />
+            <PrivateRoute
+              path='/'
+              component={HomePage}
+              isAuthenticated={this.props.auth.currentToken !== null}
+            />
+          </Switch>
         </Container>
       </BrowserRouter>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProp)(App);
