a94f41338fd2142b0a8436c649536d18eb4b96de
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 14:46:59 +0700

    pages: AuthPage: initiateAuth

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -2,11 +2,52 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
-class AuthPage extends Component<*> {
+import type { ContextRouter } from 'react-router-dom';
+
+import { initiateAuth } from '../actions/authActions';
+
+import type { StateType } from '../reducers';
+
+function mapStateToProp(state: StateType) {
+    return {
+      auth: state.auth
+    };
+}
+
+const dispatchToPropMap = {
+    initiateAuth
+};
+
+type PropsType = {
+    ...$Call<typeof mapStateToProp, *>,
+    ...typeof dispatchToPropMap,
+    ...ContextRouter
+};
+
+class AuthPage extends Component<PropsType> {
     render() {
-        return <h1>AuthPage</h1>
+        if (this.props.auth && this.props.auth.currentToken) {
+            const { from } = this.props.location.state || { from: { pathname: "/" } };
+            return <Redirect to={from} />;
+        }
+
+        const isButtonDisabled: boolean = !!(this.props.auth && this.props.auth.pendingAuth);
+
+        return (
+            <div className='text-center'>
+                <Button
+                    color='primary'
+                    disabled={isButtonDisabled}
+                    onClick={this.props.initiateAuth}
+                >
+                    Login with Medium account
+                </Button>
+            </div>
+        );
     }
 }
 
-export default AuthPage;
+export default connect(mapStateToProp, dispatchToPropMap)(AuthPage);
