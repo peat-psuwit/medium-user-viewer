830aa1814a6722813e8476c6e70ff5822cb1ac11
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-01 11:48:39 +0700

    actions: fetchAll

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,23 @@
 // @flow
 
+import { fetchUserProfile } from './userProfileActions';
+import { fetchPublicationList } from './publicationListActions';
+import { fetchAllPubContribs } from './publicationContributorsActions';
+
 import type { StateType } from '../reducers';
 
 import type { AuthActionsType } from "./authActions";
 import type { UserProfileActionsType } from './userProfileActions';
 import type { PublicationListActionsType } from './publicationListActions';
 import type { PubContribsActionsType } from './publicationContributorsActions';
+
+export function fetchAll() {
+    return function (dispatch: (ActionsType) => Promise<*>, getState: () => StateType) {
+        return dispatch(fetchUserProfile())
+            .then( () => dispatch(fetchPublicationList()))
+            .then( () => dispatch(fetchAllPubContribs()))
+    }
+}
 
 export type ActionsType = AuthActionsType
                         | UserProfileActionsType
