2bf80d40169b03fe65f3b98363d025eefb67d675
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-06-06 10:44:33 +0700

    App.test: make global test runable

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import reducer from './reducers';
 
 it('renders without crashing', () => {
+  let store = createStore(reducer);
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  , div);
   ReactDOM.unmountComponentAtNode(div);
 });
